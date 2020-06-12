5b6eb42afe6f40c6f1a906da0f3242d3bec4cd61
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2020-06-12 15:53:06 +0100

    add govuk-frontend-jinja macros

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from jinja2 import PackageLoader, PrefixLoader
 
 app = Flask(__name__)
+
+app.jinja_loader = PrefixLoader({
+    'govuk_frontend_jinja': PackageLoader('govuk_frontend_jinja', '.'),
+    'app': PackageLoader('app'),
+})
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,4 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('app/index.html')

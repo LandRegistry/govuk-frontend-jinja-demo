5ea45f7f604184fb8c03be624a942f37b4c485c0
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2020-06-19 22:14:44 +0100

    simplify

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,11 @@ from jinja2 import ChoiceLoader, PackageLoader, PrefixLoader
 
 app = Flask(__name__)
 
-loader = ChoiceLoader([
+app.jinja_loader = ChoiceLoader([
     PackageLoader('app'),
     PrefixLoader({
         'govuk_frontend_jinja': PackageLoader('govuk_frontend_jinja')
     })
 ])
-app.jinja_loader = loader
 
 from app import routes

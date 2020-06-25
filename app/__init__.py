3aa9994ea62f41176dab4324e7c6e0dad8ed4c6e
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2020-06-25 12:35:19 +0100

    change static url path

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from jinja2 import ChoiceLoader, PackageLoader, PrefixLoader
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/assets')
 
 app.jinja_loader = ChoiceLoader([
     PackageLoader('app'),

ebc9458d1c4269b16a0746bd18fead95e148a2ce
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2020-11-24 12:17:26 +0000

    black code formatting

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,10 @@ from jinja2 import ChoiceLoader, PackageLoader, PrefixLoader
 app = Flask(__name__, static_url_path="/assets")
 
 app.jinja_loader = ChoiceLoader(
-    [PackageLoader("app"), PrefixLoader({"govuk_frontend_jinja": PackageLoader("govuk_frontend_jinja")})]
+    [
+        PackageLoader("app"),
+        PrefixLoader({"govuk_frontend_jinja": PackageLoader("govuk_frontend_jinja")}),
+    ]
 )
 
 from app import routes

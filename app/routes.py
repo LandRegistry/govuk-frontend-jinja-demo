fc224425e4925c3a0f4cd2dcb3d97fe0179593b5
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-01-19 23:51:31 +0000

    start component fixture page

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import render_template
 
 from app import app
@@ -6,3 +8,11 @@ from app import app
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/components/<string:component>")
+def component(component):
+    with open("govuk_components/{}/fixtures.json".format(component)) as json_file:
+        fixtures = json.load(json_file)
+
+    return render_template("component.html", fixtures=fixtures)

fc52643c681aaafe9e857aaea7103d5671e70189
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-01-20 00:05:52 +0000

    components index page

--- app/routes.py
+++ app/routes.py
@@ -10,6 +10,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/components")
+def components():
+    return render_template("components.html")
+
+
 @app.route("/components/<string:component>")
 def component(component):
     with open("govuk_components/{}/fixtures.json".format(component)) as json_file:

986eea039ae84d566f72386e39fb963a9625db80
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-01-20 23:26:05 +0000

    remove components index page

--- app/routes.py
+++ app/routes.py
@@ -10,11 +10,6 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/components")
-def components():
-    return render_template("components.html")
-
-
 @app.route("/components/<string:component>")
 def component(component):
     with open("govuk_components/{}/fixtures.json".format(component)) as json_file:
